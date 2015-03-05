dca91c1f5e79dbd8a1faede1a7e362b75ea6a006
calibr <awcalibr@gmail.com>
2015-03-05 20:49:16 +0300

    initial

---
+++ test/parse-files.js
@@ -0,0 +1,29 @@
+var
+  should = require("should"),
+  parse = require("../lib/bookmarks-parser"),
+  fs = require("fs"),
+  util = require("util");
+
+describe("Parse bookmarks", function() {
+  it("netscape file", function(done) {
+    var html = fs.readFileSync(__dirname + "/files/netscape.html", "utf-8");
+    var result = fs.readFileSync(__dirname + "/files/netscape.json", "utf-8");
+    parse(html, function(err, res) {
+      should.not.exists(err);
+      res.parser.should.equal("netscape");
+      JSON.stringify(res.bookmarks).should.equal(result);
+      done();
+    });
+  });
+
+  it("pocket file", function(done) {
+    var html = fs.readFileSync(__dirname + "/files/pocket.html", "utf-8");
+    var result = fs.readFileSync(__dirname + "/files/pocket.json", "utf-8");
+    parse(html, function(err, res) {
+      should.not.exists(err);
+      res.parser.should.equal("pocket");
+      JSON.stringify(res.bookmarks).should.equal(result);
+      done();
+    });
+  });
+});
